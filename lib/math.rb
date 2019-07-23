810ce123a50c72054d61189a2ef56292ce65ef14
Christopher Kennedy <chris94kennedy@gmail.com>
2019-07-23 13:24:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,30 +2,23 @@ def addition(num1, num2)
   num1 + num2
 end
 
-
 def subtraction(num1, num2)
   num1 - num2
 end
-
-
 
 def division(num1, num2)
   num1 / num2
 end
 
-
-
 def multiplication(num1, num2)
   num1 * num2
 end
-
 
 def modulo(num1, num2)
   num1 % num2 
 end
 #Modulo operator returns the REMAINDER of the divison as opposed
-
-
+#to the number yielded by the division itself
 
 def square_root(num)
  Math.sqrt(num)
